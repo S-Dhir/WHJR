ae09cbd419bcab8520e5736a334c3e6438ba57f5
Dark-lord20 <73019620+Dark-lord20@users.noreply.github.com>
2020-12-09 17:39:18 +0530

    c

--- My Favourite-2/main.js
+++
@@ -1,8 +0,0 @@
-function collaps(){
-    if (document.getElementById("col").style.opacity==="0"){
-      document.getElementById("col").style.opacity="1"
-    }
-    else {
-      document.getElementById("col").style.opacity="0"
-    }
-  }

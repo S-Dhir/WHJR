465e014f84cb1853b2d26d061aa1303734d2159e
Dark-lord20 <73019620+Dark-lord20@users.noreply.github.com>
2021-03-09 17:54:06 +0530

    School website

---
+++ School website/index.js
@@ -0,0 +1,6 @@
+/*home,contact,About,Curriculum,Sports*/
+function contact(consol) {
+    if (consol=="contact") {
+        document.getElementById("sections").innerHTML='<iframe class="maps-frame" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4530458.203412643!2d-7.303289848678839!3d56.32745238132332!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4861e2c403f2a19f%3A0xe7c1fad809c30714!2sScotland%2C%20UK!5e0!3m2!1sen!2sin!4v1615292232810!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="true" loading="lazy"></iframe>';
+    }
+}
